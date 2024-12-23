1ce9969150d02398cde93c2568fd1c954f25f573
andrey_brysin <andrey13brysin@gmail.com>
2024-12-23 10:33:30 +0300

    String_New_vers

--- Interpolation.cs
+++ Interpolation.cs
@@ -9,74 +9,21 @@ namespace CS_JUN_1
 	{
 		static void Main(string[] args)
 		{
-			string[] enters = new string[10];
-			string[] variants = { "НЕТ", "ДА" };
+			Console.Write("как вас зовут? ");
+			string name = Console.ReadLine();
 
-			Console.WriteLine("Здравствуйте, вы человек? ДА/НЕТ");
-			enters[0] = Rep_Q(variants);
-			if (enters[0].ToLower() == "да")
-			{
-				
-			}
-			else
-			{
-				Console.WriteLine("А кто вы???? (-_-)");
-				Console.ReadLine();
-				Console.WriteLine("Соболезную, досвидания");
-				Console.ReadLine();
-				return;
-			}
+			Console.Write("где вы работаете? ");
+			string job = Console.ReadLine();
 
+			Console.Write("вы много знаете? ");
+			string know = Console.ReadLine();
 
-			Console.WriteLine("вы мужчина? ДА/НЕТ");
-			enters[1] = Rep_Q(variants);
-			if (enters[0].ToLower() == "да")
-			{
-				Random r = new Random();
-				if((r.NextDouble() % 1.0) > 0.95)
-				{
-					if(enters[0].ToLower() == "да")
-					{
-						Console.WriteLine("проигрывал?");
-						Console.WriteLine("ДА");
-						Console.WriteLine("круто я тоже");
-						Console.ReadKey();
-					}
-					else
-					{
-						Console.Clear();
-						Console.WriteLine("тут и так понятно");
-						Console.ReadKey();
-					}
-					return;
-				}
-			}
+			Console.Write("как вы спите? ");
+			string sleep = Console.ReadLine();
 
-			Console.WriteLine("как тебя зовут?");
-			enters[2] = Console.ReadLine();
+			System.Console.WriteLine($"Вас зовут {name}, вы работаете {job}, вы {know} знаете и {sleep} спите");
 
-			Console.WriteLine("вы успешный програмист и делаете игры ?");
-
-			enters[3] = Rep_Q(variants);
-
-			if (enters[3].ToLower() == "да")
-			{
-				Console.WriteLine("тогда предлагаю вам не тратить время, и купить проверку этой программы (ENTER)");
-				enters[4] = Console.ReadLine();
-				if (enters[4] == "\n")
-				{
-					Console.WriteLine("удачи " + enters[2]);
-					return;
-				}
-			}
-			else
-			{
-				string[] tt = { "gfhjkm" };
-				Console.WriteLine("ладно тогда введите пароль для продолжения");
-				Rep_Q(tt);
-				Console.WriteLine("вы человек " + (enters[1] == "ДА" ? "Муржского" : "Женского") + " пола, по имени " + enters[2] + " и вы не успешный програмист, в чем я вам сильно сочувствую*");
-				Console.ReadLine();
-			}
+			Сonsole.ReadKey();
 		}
 	}
 }
